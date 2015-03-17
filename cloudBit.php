d86317487285eb112e83f869ff6b16fef390e2e7
Nimrod Lahav <nimrodl@wix.com>
2015-03-17 21:27:02 +0200

    add code

---
+++ cloudBit.php
@@ -0,0 +1,19 @@
+<?php
+function sendToCloudBit($cloudBitKey, $deviceId, $durationInMillis, $valueType, $value){
+	$url = "https://api-http.littlebitscloud.cc/v3/devices/$deviceId/output";
+	$data = array($valueType => $value, 'duration_ms' => $durationInMillis);
+	
+	$options = array(
+			'http' => array(
+					'header'  => "Authorization: Bearer $cloudBitKey\r\n",
+					'method'  => 'POST',
+					'content' => http_build_query($data),
+			),
+	);
+	
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+	
+	echo "<br>Little Bit said: $result<br>";
+}
+?>
