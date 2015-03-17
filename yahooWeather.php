bdda0319b626a914f437af862a3a864fc496d69d
Nimrod Lahav <nimrodl@wix.com>
2015-03-17 22:03:37 +0200

    weather

---
+++ yahooWeather.php
@@ -0,0 +1,21 @@
+<?php
+include('cloudBit.php');
+echo "<h3>Yahoo Weather In Little Bit</h3>";
+
+$city = urlencode("tel aviv");
+$countryCode = urlencode("il");
+$metricsInFahrenheit = true;
+
+$metricUrl="";
+if (!$metricsInFahrenheit){
+  $metricUrl = urlencode(" and u='c'");
+}
+
+$yahooApiURL = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22$city%2C%20$countryCode%22)$metricUrl&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+$jsonFromYahoo  = json_decode(file_get_contents($yahooApiURL),true);
+
+$tmp = $jsonFromYahoo['query']['results']['channel']['item']['condition']['temp'];
+echo "<h4>The temp is:$tmp </h4>";
+
+?>
